cc32d500289b45564d5d22149abb997bfb2b2584
priiyaanjaalii0611 <priyanjali.gupta2019@vitstudent.ac.in>
2021-08-27 14:54:04 +0530

    modified keys

--- keyboard.py
+++ keyboard.py
@@ -2,6 +2,9 @@ import cv2
 import sys
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
+import numpy as np
+import cvzone
+
 
 cap =cv2.VideoCapture(0)
 cap.set(3,1280)
@@ -13,11 +16,19 @@ keys=[["Q","w","E","R","T","Y","U","I","O","P"],
 ["A","S","D","F","G","H","J","K","L",";"],
 ["Z","X","C","V","B","N","M",",",".","/"]]
 def drawAll(img,buttonList):
+    imgNew=np.zeros_like(img,np.uint8)
+
     for button in buttonList:
         x,y=button.pos 
-        w,h=button.size   
-        cv2.rectangle(img,button.pos,(x+w,y+h),(255,0,253),cv2.FILLED)
-        cv2.putText(img,button.text,(x+20,y+65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)
+        cvzone.cornerRect(imgNew,(button.pos[0],button.pos[1],button.size[0],button.size[1]),20,rt=0)   
+        cv2.rectangle(imgNew,button.pos,(x+button.size[0],y+button.size[1]),(255,0,253),cv2.FILLED)
+        cv2.putText(img,button.text,(x+20,y+65),cv2.FONT_HERSHEY_PLAIN,2,(255,255,255),4)
+    
+    out=img.copy()
+    alpha=0.5
+    mask=imgNew.astype(bool)
+    print(mask.shape)
+    out[mask]=cv2.addWeighted(img,alpha,imgNew,1-alpha,0)[mask]
     return img    
 
 class Button():
@@ -49,13 +60,14 @@ while True:
             if x<lmlist[8][0]<x+w and y<lmlist[8][1]<y+h:
                 cv2.rectangle(img,button.pos,(x+w,y+h),(155,0,200),cv2.FILLED)
                 cv2.putText(img,button.text,(x+20,y+65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)
-                l,_,_=detector.findDistance(8,12,img)   
-                print(l)
+                l,_,_=detector.findDistance(8,12,img,draw=False)   
+                
 
                 if l< 45:
                     cv2.rectangle(img,button.pos,(x+w,y+h),(255,0,0),cv2.FILLED)           
                     cv2.putText(img,button.text,(x+20,y+65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)  
-                    finalText+=button.text  
+                    finalText+=button.text
+                    sleep(0.15)  
              
                            
     
